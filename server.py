e1598b9c9ef3f58e163b465f756490de53befcc9
pedrohlopes <pedrohlops@gmail.com>
2022-04-06 12:37:16 -0300

    update models

--- server.py
+++ server.py
@@ -18,6 +18,28 @@ from audio_processing import griffin_lim
 from train import load_model
 from text import text_to_sequence
 from denoiser import Denoiser
+import noisereduce as nr
+from num2words import num2words
+import re
+
+def fix_periods(str):
+    if str == "":                    # Don't change empty strings.
+        return str
+    if str[-1] in ["?", ".", "!"]:   # Don't change if already okay.
+        return str
+    if str[-1] == ",":               # Change trailing ',' to '.'.
+        return str[:-1] + "."
+    return str + "."                 # Otherwise, add '.'.
+
+def convert_helper(obj):
+    num_in_text = int(obj.group(0))
+    return num2words(num_in_text,lang='pt_BR').replace(",","")
+
+def convert_numbers(text):
+    return re.sub(r'\d+',convert_helper,text)
+
+
+
 hparams = create_hparams()
 
 hparams.sampling_rate=22050
@@ -30,26 +52,26 @@ hparams.mel_fmax=8000.0
 hparams.global_mean = None
 hparams.max_decoder_steps=2000
 hparams.idx = 0
-tacotron_path_base = "/home/ubuntu/voice_code/tacotron-mod/tacotron2/outdir/checkpoint_from_grid02"
-waveglow_path_base = '/home/ubuntu/voice_code/waveglow/checkpoints/best_22050_base'
-tacotron_path_cadu = "/home/ubuntu/voice_code/tacotron-mod/tacotron2/outdir/checkpoint_from_grid01"
-waveglow_path_cadu = '/home/ubuntu/voice_code/waveglow_grid/waveglow/checkpoints/waveglow_18900'
+tacotron_path_base = "/home/ubuntu/tts_models/tacotron/latest_voz_base_22"
+waveglow_path_base = '/home/ubuntu/voice_code/tacotron-mod/tacotron2/outdir/waveglow_base_22_cond'
+tacotron_path_dirceu = "/home/ubuntu/voice_code/tacotron-mod/tacotron2/outdir/last_dirceu" #checkpoint_from_grid01 era o cadu
+waveglow_path_dirceu = '/home/ubuntu/voice_code/tacotron-mod/tacotron2/outdir/checkpoint_dirceu'
 def load_synth_model(tacotron_path,waveglow_path):
     model = load_model(hparams)
     model.load_state_dict(torch.load(tacotron_path)['state_dict'])
     _ = model.cuda().eval().half()
     waveglow = torch.load(waveglow_path)['model']
     waveglow.cuda().eval().half()
-    current_model = 'base'
+    current_model = 'dirceu'
     for k in waveglow.convinv:
         k.float()
     denoiser = Denoiser(waveglow,filter_length=hparams.filter_length, n_overlap=4,
                             win_length=hparams.win_length, mode='zeros',n_mels=hparams.n_mel_channels)
     return model, waveglow, denoiser
     
-model, waveglow, denoiser = load_synth_model(tacotron_path_base,waveglow_path_base)
-current_model = 'voz_base'
-current_sigma = 0.8
+model, waveglow, denoiser = load_synth_model(tacotron_path_dirceu,waveglow_path_dirceu)
+current_model = 'dirceu'
+current_sigma = 0.93
 current_denoiser_alpha = 0.01
 
 app = Flask(__name__,static_url_path='')
@@ -64,7 +86,7 @@ def upload_file2():
     global current_model, model, current_denoiser_alpha,current_sigma, waveglow, denoiser
     if request.method == 'POST':
         print('debug')
-        synth_text = request.form.get("synth_text")
+        synth_text = fix_periods(convert_numbers(request.form.get("synth_text")))
         selected_model = request.form.get("select_model")
         print(f'Selected model : {selected_model}')
         print(synth_text)
@@ -72,11 +94,11 @@ def upload_file2():
             current_model = selected_model
             if selected_model == 'voz_base':
                 model, waveglow, denoiser = load_synth_model(tacotron_path_base,waveglow_path_base)
-                current_sigma = 0.8
+                current_sigma = .93
                 current_denoiser_alpha = 0.01  
-            elif selected_model=='cadu_2h':
-                model, waveglow, denoiser = load_synth_model(tacotron_path_cadu,waveglow_path_cadu)
-                current_sigma = 0.7
+            elif selected_model=='dirceu':
+                model, waveglow, denoiser = load_synth_model(tacotron_path_dirceu,waveglow_path_dirceu)
+                current_sigma = 0.8
                 current_denoiser_alpha = 0.1 
         
                 
@@ -89,9 +111,9 @@ def upload_file2():
             torch.from_numpy(sequence)).cuda().long()
         mel_outputs, mel_outputs_postnet, _, alignments = model.inference(sequence)
         audio = waveglow.infer(mel_outputs_postnet, sigma=current_sigma)
-        audio_denoised = denoiser(audio, strength=current_denoiser_alpha)[:, 0]
+        audio_denoised = nr.reduce_noise(y=audio[0].data.cpu().numpy(), sr=hparams.sampling_rate, prop_decrease=0.7)
         hparams.idx+=1
-        af.write(f'audio_result{hparams.idx}.wav',audio_denoised.cpu().numpy(),22050)
+        af.write(f'audio_result{hparams.idx}.wav',audio_denoised.astype(np.float32),22050)
         
         return render_template('upload.html', audio_filename=f'audio_result{hparams.idx}.wav')
 
